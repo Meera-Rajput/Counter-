368054f577e3e2dd9a564acc7d71446a584ab074
Meera-Rajput <110844782+Meera-Rajput@users.noreply.github.com>
2022-08-08 23:25:09 +0530

    Add files via upload
    Commit 3

---
+++ meera counter/index.js
@@ -0,0 +1,16 @@
+let counter = 0
+
+function subtract(){
+counter--
+console.log(document.getElementById("zero").innerHTML = counter);
+
+}
+function reset(){
+    counter = 0
+    console.log(document.getElementById("zero").innerHTML = counter);
+
+}
+function addition(){
+    counter++
+    console.log(document.getElementById("zero").innerHTML = counter);
+    }
